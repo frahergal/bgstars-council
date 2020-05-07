a979189ad44293a50538899e0de3a259872b8869
Francisco Javier Hernández <frahergal@users.noreply.github.com>
2020-05-07 22:40:14 +0200

    chore(council): add maven project

---
+++ src/main/java/eu/journeytospace/bgstars/council/BurguerStarsUsersCouncilApplication.java
@@ -0,0 +1,13 @@
+package eu.journeytospace.bgstars.council;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurguerStarsUsersCouncilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurguerStarsUsersCouncilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/journeytospace/bgstars/council/BurguerStarsUsersCouncilApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.journeytospace.bgstars.council;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurguerStarsUsersCouncilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
